8d21bf0b979ffab49212d03d09b671e96c0cbcb7
SM Sadat <smsadat788@gmail.com>
2025-08-15 00:31:31 +0600

    feat: Implement Delete and Update

--- ToDo/base/views.py
+++ ToDo/base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from .models import TaskModel
 
@@ -8,22 +8,68 @@ from .models import TaskModel
 def processTasks(request):
 
     if request.method == 'POST':
-        TaskTitle = request.POST.get('tasksTitle')
-        TaskDesc = request.POST.get('tasksDescrp')
-        TaskStatus = request.POST.get('isTaskDone')
+        action = request.POST.get('action')
+    # ---------------- CREATE TASK ---------------- #
+        if action == 'create':
 
-        IsDone = True if TaskStatus == 'on' else False
+            TaskTitle = request.POST.get('tasksTitle')
+            TaskDesc = request.POST.get('tasksDescrp')
+            TaskStatus = request.POST.get('isTaskDone')
 
-        task = TaskModel(
-            taskTitle = TaskTitle,
-            taskDescription = TaskDesc,
-            isDone =  IsDone
-        )
-        task.save()
+            IsDone = True if TaskStatus == 'on' else False
 
-        # redirect to same page to prevent resubmission
-        return redirect('base:task')
+            task = TaskModel(
+                taskTitle = TaskTitle,
+                taskDescription = TaskDesc,
+                isDone =  IsDone
+            )
+            task.save()
 
+            # return JSON model
+            return JsonResponse({
+                'status' : 'Success',
+                'message': 'Task created successfully',
+                'task': {
+                    'id': task.id,
+                    'taskTitle': task.taskTitle,
+                    'taskDescription': task.taskDescription,
+                    'creationTime': str(task.creationTime),
+                    'isDone': task.isDone,
+                }  
+            })
+        
+    # ---------------- UPDATE TASK ---------------- #
+
+        elif action == 'update':    
+            task_id = request.POST.get('task_id')
+            is_done = request.POST.get('is_done') == 'true'
+
+            try:
+                task = TaskModel.objects.get(id=task_id)
+                task.isDone = is_done
+                task.save()
+                return JsonResponse({'status': 'Success', 'message': 'Task updated'})
+
+            except TaskModel.DoesNotExist:
+                return JsonResponse({'status': 'Error', 'message': 'Task not found'})
+
+    # ---------------- DELETE TASK ---------------- #
+
+        elif action == 'delete':
+            task_id = request.POST.get('task_id')
+
+            try:
+                task = TaskModel.objects.get(id=task_id)
+                task.delete()
+                return JsonResponse({'status': 'Success', 'message': 'Task deleted'})
+
+            except TaskModel.DoesNotExist:
+                return JsonResponse({'status': 'Error', 'message': 'Task not found'})
+
+        else:
+            return JsonResponse({'status': 'error', 'message': 'Invalid action'})
+
+    # ---------------- GET TASKS (LIST) ---------------- #
     if request.method == 'GET':
         tasks = TaskModel.objects.all()
         return render(request, 'taskview.html', {'tasks' : tasks})
